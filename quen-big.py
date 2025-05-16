0183f8ddab8863b1a5379ae75610ae0d854b6b7c
Your Name <you@example.com>
2025-05-16 13:57:59 +0000

    updated requirements to more stable versions

--- quen-big.py
+++ quen-big.py
@@ -30,18 +30,18 @@ prompt = """
    Use max_length=7000 for the generate method of the model.
    
    Create a form with a text area to submit a prompt to the model.
-   Also include text inputs for the model parameters top_k, temperature and do_sample.
+   Also include text inputs for the model parameters top_k, top_p, temperature and do_sample.
    Pass those parameters to the generate method of the model.
    Provide the endpoint for the form under the root path /. 
    Return the HTML directly as string in the response of the http request.
-   """
+"""
 
 # tokenize the text prompt. Put the tokenized input
 # on the same device as the model (cpu or gpu)
 inputs = tokenizer(prompt, return_tensors='pt').to(model.device)
 
 # generate the output with the model
-outputs = model.generate(**inputs, do_sample=false, max_length=7000)
+outputs = model.generate(**inputs, do_sample=False, max_length=7000)
 
 # decode the output with the tokenizer
 result = tokenizer.decode(outputs[0], skip_special_tokens=True)
